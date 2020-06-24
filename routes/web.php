a0eb1011d70240f3dbe2d3cb4a7ec1c37c450e64
zerckel <antoinecarbonnel@yahoo.fr>
2020-06-24 10:50:49 +0200

    cors

--- routes/web.php
+++ routes/web.php
@@ -14,54 +14,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'indexController@index');
-Route::get('/basket/', 'basketController@index');
-Route::get('/contact/', 'contactController@index');
-Route::get('/brands/', 'catalogController@index');
-Route::get('/brands/{id}', 'catalogController@brandsId');
-Route::get('/search/{search}', 'SearchController@index');
-Route::get('/product/{id}', 'productsheet@index');
-Route::get('/news/', 'newssheet@indexList');
-Route::get('/news/{id}', 'newssheet@index');
+Route::middleware('cors')->group(function () {
 
-Route::prefix('tool')->group(function () {
-    Route::get('addToBasket', 'toolBox@addToBasket');
-});
+    Route::get('/', 'indexController@index');
+    Route::get('/basket/', 'basketController@index');
+    Route::get('/contact/', 'contactController@index');
+    Route::get('/brands/', 'catalogController@index');
+    Route::get('/brands/{id}', 'catalogController@brandsId');
+    Route::get('/search/{search}', 'SearchController@index');
+    Route::get('/product/{id}', 'productsheet@index');
+    Route::get('/news/', 'newssheet@indexList');
+    Route::get('/news/{id}', 'newssheet@index');
 
-Route::post('/sendmail/', 'mailController@index');
-Route::post('/confirmation/', 'confirmation@index');
-Route::get('/confirmation/', 'confirmation@finalPage');
+    Route::prefix('tool')->group(function () {
+        Route::get('addToBasket', 'toolBox@addToBasket');
+    });
 
-Auth::routes();
+    Route::post('/sendmail/', 'mailController@index');
+    Route::post('/confirmation/', 'confirmation@index');
+    Route::get('/confirmation/', 'confirmation@finalPage');
 
-Route::prefix('admin')->group(function () {
-    Route::get('/', 'administratorController@index')->name('admin');
+    Auth::routes();
+
+    Route::prefix('admin')->group(function () {
+        Route::get('/', 'administratorController@index')->name('admin');
 
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/logout', 'administratorController@logout');
+        Route::middleware('auth')->group(function () {
+            Route::get('/logout', 'administratorController@logout');
 
-        Route::delete('/products/{id}', 'administratorController@deleteProducts');
-        Route::delete('/news/{id}', 'administratorController@deleteNews');
-        Route::delete('/brands/{id}', 'administratorController@deleteBrands');
+            Route::delete('/products/{id}', 'administratorController@deleteProducts');
+            Route::delete('/news/{id}', 'administratorController@deleteNews');
+            Route::delete('/brands/{id}', 'administratorController@deleteBrands');
 
-        Route::get('/brands', 'administratorController@brands');
-        Route::get('/products', 'administratorController@products');
-        Route::get('/news', 'administratorController@news');
-        Route::get('/brands/add', 'brandsController@index');
-        Route::get('/products/add', 'productsController@index');
-        Route::get('/news/add', 'newsController@index');
+            Route::get('/brands', 'administratorController@brands');
+            Route::get('/products', 'administratorController@products');
+            Route::get('/news', 'administratorController@news');
+            Route::get('/brands/add', 'brandsController@index');
+            Route::get('/products/add', 'productsController@index');
+            Route::get('/news/add', 'newsController@index');
 
-        Route::post('/brands/add', 'brandsController@addBrand');
-        Route::post('/products/add', 'productsController@addProduct');
-        Route::post('/news/add', 'newsController@addNews');
+            Route::post('/brands/add', 'brandsController@addBrand');
+            Route::post('/products/add', 'productsController@addProduct');
+            Route::post('/news/add', 'newsController@addNews');
 
-        Route::get('/brands/{id}', 'brandsController@update');
-        Route::get('/products/{id}', 'productsController@update');
-        Route::get('/news/{id}', 'newsController@update');
+            Route::get('/brands/{id}', 'brandsController@update');
+            Route::get('/products/{id}', 'productsController@update');
+            Route::get('/news/{id}', 'newsController@update');
 
-        Route::put('/brands/{id}', 'brandsController@updateBrand');
-        Route::put('/products/{id}', 'productsController@updateProduct');
-        Route::put('/news/{id}', 'newsController@updateNews');
+            Route::put('/brands/{id}', 'brandsController@updateBrand');
+            Route::put('/products/{id}', 'productsController@updateProduct');
+            Route::put('/news/{id}', 'newsController@updateNews');
+        });
     });
 });
